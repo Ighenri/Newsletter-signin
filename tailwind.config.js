fdd3ef3efce144286aa8434a6875231ee2a0cdbd
Henry <igwehenry740@gmail.com>
2023-06-21 16:04:32 +0100

    mobileview copleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,14 @@ module.exports = {
   theme: {
       colors:{
         'Tomato': 'hsl(4, 100%, 67%)',
-        'Dark Slate Grey': 'hsl(234, 29%, 20%)',
-        'Charcoal Grey': 'hsl(235, 18%, 26%)',
+        'DarkSlateGrey': 'hsl(234, 29%, 20%)',
+        'CharcoalGrey': 'hsl(235, 18%, 26%)',
         'Grey': 'hsl(231, 7%, 60%)',
         'White': 'hsl(0, 0%, 100%)',
       },
       fontFamily:{
         roboto: ['Roboto', 'sans-serif'],
       },
-      screens: {
-      'sm': '200px',
-      // => @media (min-width: 200px) { ... }
-
-      'md': '960px',
-      // => @media (min-width: 960px) { ... }
-
-      'lg': '1440px',
-      // => @media (min-width: 1440px) { ... }
-    },
     extend: {
 
     },
